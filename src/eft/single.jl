b0f72a171b334ac78058ffaebe80e1bf2be02516
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2015-11-07 18:44:55 -0500

    Update single.jl

--- src/eft/single.jl
+++ src/eft/single.jl
@@ -1,10 +1,10 @@
-@inline function eftSquare(a::FloatingPoint)
+@inline function eftSquare(a::AbstractFloat)
     x = a * a
     y = fma(a, a, -x)
     x,y
 end
 
-function eftCube(a::FloatingPoint)
+function eftCube(a::AbstractFloat)
     p = a*a; e = fma(a, a, -p)
     x = p*a; p = fma(p, a, -x)
     y = e*a
@@ -15,7 +15,7 @@ end
 # presently 'y' must be negated to get the right result
 # I do not know why.
 #
-@inline function eftRecip(a::FloatingPoint)
+@inline function eftRecip(a::AbstractFloat)
      x = one(Float64)/a
      y = fma(x,a,-1.0)/a
      x,-y
@@ -26,14 +26,14 @@ end
    Augmented precision square roots, 2-D norms and discussion on correctly reounding sqrt(x^2 + y^2)
    by Nicolas Brisebarre, Mioara Joldes, Erik Martin-Dorel, Hean-Michel Muller, Peter Kornerup
 =#
-@inline function eftSqrt(a::FloatingPoint)
+@inline function eftSqrt(a::AbstractFloat)
      x = sqrt(a)
      t = fma(x,-x,a)
      y = t / (x*2.0)
      x,y
 end     
      
-@inline function eftRecipSqrt(a::FloatingPoint)
+@inline function eftRecipSqrt(a::AbstractFloat)
      r = 1.0/a
      x = sqrt(r)
      t = fma(x,-x,r)
