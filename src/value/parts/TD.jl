b992d478792bed4b4044db915b6151d82d07aa32
J-Sarnoff <jeffreysarnoff@gmail.com>
2015-11-20 16:58:03 -0500

    Update TD.jl

--- src/value/parts/TD.jl
+++ src/value/parts/TD.jl
@@ -1,10 +1,11 @@
-function ldexp(a::TD,xp::Int)
-    TD(ldexp(a.hi,xp),ldexp(a.md,xp),ldexp(a.lo,xp))
-end
-
 function frexp(a::TD)
     frhi, xphi = frexp(a.hi)
     frmd, xpmd = frexp(a.md)
     frlo, xplo = frexp(a.lo)
     TD(frhi, ldexp(frmd,xpmd-xphi), ldexp(frlo,xplo-xpmd-xphi)), (xphi, xpmd, xplo)
 end
+
+function ldexp(a::TD,xp::Int)
+    TD(ldexp(a.hi,xp),ldexp(a.md,xp),ldexp(a.lo,xp))
+end
+ldexp{I<:Integer}(fx::Tuple{TD,I}) = ldexp(fx...)
