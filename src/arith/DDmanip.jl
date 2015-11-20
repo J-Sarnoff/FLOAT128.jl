ba5a8c694190ba6d196cb51fa3c9fd026c36e838
J-Sarnoff <jeffreysarnoff@gmail.com>
2015-11-20 02:17:01 -0500

    Update DDmanip.jl

--- src/arith/DDmanip.jl
+++ src/arith/DDmanip.jl
@@ -23,18 +23,26 @@ end
 @inline copysign(a::DD,b::DD) = copysign(a,b.hi)
 
 
-function (floorceil)(fn::Function, a::DD)
-    hi = fn(a.hi)
-    lo = zero(Float64)
+
+function (floor)(a::DD)
+    hi = floor(a.hi)
+    lo = 0.0
     if (hi == a.hi)
-        lo = fn(a.lo)
+        lo = floor(a.lo)
         hi,lo = eftSum2inOrder(hi,lo)
     end
     DD(hi,lo)
 end
 
-(floor)(a::DD) = floorceil(floor,a)
-(ceil)(a::DD) = floorceil(ceil,a)
+function (ceil)(a::DD)
+    hi = ceil(a.hi)
+    lo = 0.0
+    if (hi == a.hi)
+        lo = ceil(a.lo)
+        hi,lo = eftSum2inOrder(hi,lo)
+    end
+    DD(hi,lo)
+end
 
 
 function round(a::DD)
@@ -46,33 +54,17 @@ function round(a::DD)
     end
     DD(hi,lo)
 end
-#(round)(a::DD) = floorceil(round,a)
 
-@inline function (trunc)(a::DD)
-    a.hi >= zero(Float64) ? floor(a) : ceil(a)
+function (trunc)(a::DD)
+    a.hi >= zero(F) ? floor(a) : ceil(a)
 end
 
 """
 stretch is the opposite of trunc()
 it extends to the nearest integer away from zero
 """
-@inline function (stretch)(a::DD)
+function (stretch)(a::DD)
     a.hi >= zero(Float64) ? ceil(a) : floor(a)
-end
-
-
-function fld{T<:DD}(a::T,b::T)
-    if (b.hi == zero(Float64))
-        throw(DomainError("denominator must be nonzero"))
-    end
-    floor( a/b )
-end
-
-function cld{T<:DD}(a::T,b::T)
-    if (b.hi == zero(Float64))
-        throw(DomainError("denominator must be nonzero"))
-    end
-    ceil( a/b )
 end
 
 
@@ -103,6 +95,24 @@ end
 div(a::DD,b::Float64) = div(a,DD(b))
 div(a::Float64,b::DD) = div(DD(a),b)
 
+function fld{T<:DD}(a::T,b::T)
+    if (b.hi == zero(Float64))
+        throw(DomainError("denominator must be nonzero"))
+    end
+    floor( a/b )
+end
+fld(a::DD,b::Float64) = fld(a,DD(b))
+fld(a::Float64,b::DD) = fld(DD(a),b)
+
+function cld{T<:DD}(a::T,b::T)
+    if (b.hi == zero(Float64))
+        throw(DomainError("denominator must be nonzero"))
+    end
+    ceil( a/b )
+end
+cld(a::DD,b::Float64) = cld(a,DD(b))
+cld(a::Float64,b::DD) = cld(DD(a),b)
+
 function rem{T<:DD}(a::T,b::T)
     if (b.hi == zero(Float64))
         throw(DomainError("denominator must be nonzero"))
@@ -110,6 +120,19 @@ function rem{T<:DD}(a::T,b::T)
     a - div(a,b)*b
 end
 
+function mod{T<:DD}(a::T,b::T)
+    if (b.hi == zero(Float64))
+        throw(DomainError("modulus must be nonzero"))
+    end
+    if signbit(a.hi)==signbit(b.hi)
+        rem(a,b)
+    else
+        d = floor(a/b)
+        a - d*b
+    end
+end
+mod(a::DD,b::Float64) = mod(a,convert(DD,b))
+mod(a::Float64,b::DD) = mod(convert(DD,a),b)
 
 %{T<:TD}(a::T,b::T) = a - trunc(a/b)
 %(a::TD,b::Float64) = %(a,b)
@@ -124,28 +147,16 @@ function divrem{T<:DD}(a::T,b::T)
     r = a - d*b
     d,r
 end
-
-
-function mod{T<:DD}(a::T,b::T)
-    if (b.hi == zero(Float64))
-        throw(DomainError("modulus must be nonzero"))
-    end
-    if signbit(a.hi)==signbit(b.hi)
-        rem(a,b)
-    else
-        d = floor(a/b)
-        a - d*b
-    end
-end
-
-mod(a::DD,b::Float64) = mod(a,convert(DD,b))
-mod(a::Float64,b::DD) = mod(convert(DD,a),b)
+divrem(a::DD, b::Float64) = divrem(a,convert(DD,b))
+divrem(a::Float64, b::DD) = divrem(convert(DD,a),b)
 
 function fldmod{T<:DD}(a::T,b::T)
     d = floor(a/b)
     a - d*b
     d,a
 end
+fldmod(a::DD, b::Float64) = fldmod(a,convert(DD,b))
+fldmod(a::Float64, b::DD) = fldmod(convert(DD,a),b)
 
 #=
    This well-behaved bounded modulo implementation is from
@@ -157,4 +168,6 @@ function modulo{T<:DD}(a::T, lowerbound::T, upperbound::T)
     delta = upperbound - lowerbound
     a - (floor((a - lowerbound)/delta) * delta)
 end
+modulo(a::DD, lowerbound::Float64, upperbound::Float64) = modulo(a,convert(DD,lowerbound),convert(DD,upperbound))
+modulo(a::Float64, lowerbound::DD, upperbound::DD) = modulo(convert(DD,a),lowerbound,upperbound)
 
