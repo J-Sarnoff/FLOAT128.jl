00a185252aeb3afb13225d3c1e77e25773867180
J-Sarnoff <jeffreysarnoff@gmail.com>
2015-11-20 16:30:16 -0500

    Update TD.jl

--- src/value/parts/TD.jl
+++ src/value/parts/TD.jl
@@ -2,7 +2,7 @@ function ldexp(a::TD,xp::Int)
     TD(ldexp(a.hi,xp),ldexp(a.md,xp),ldexp(a.lo,xp))
 end
 
-function frexp(a::DD)
+function frexp(a::TD)
     frhi, xphi = frexp(a.hi)
     frmd, xpmd = frexp(a.md)
     frlo, xplo = frexp(a.lo)
