539557a8e9ce4af0adf141d120006bb6972880d0
J-Sarnoff <jeffreysarnoff@gmail.com>
2015-11-20 02:06:32 -0500

    Update DD.jl

--- src/value/cmp/DD.jl
+++ src/value/cmp/DD.jl
@@ -49,13 +49,3 @@ end
 @inline function (trunc)(a::DD)
     a.hi >= zero(F) ? floor(a) : ceil(a)
 end
-
-function ldexp(a::DD,xp::Int)
-    DD(ldexp(a.hi,xp),ldexp(a.lo,xp))
-end
-
-function frexp(a::DD)
-    frhi, xphi = frexp(a.hi)
-    frlo, xplo = frexp(a.lo)
-    DD(frhi, ldexp(frlo,xplo-xphi)), xphi
-end
