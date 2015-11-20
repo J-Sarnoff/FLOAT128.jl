53007ef2f39b9721143eeb106777c7b9496b219a
J-Sarnoff <jeffreysarnoff@gmail.com>
2015-11-20 02:31:36 -0500

    Update TD.jl

--- src/value/cmp/TD.jl
+++ src/value/cmp/TD.jl
@@ -1,5 +1,13 @@
 # predicates
 
-isnan(a::TD) = isnan(a.hi)
-isinf(a::TD) = isinf(a.hi)
-isfinite(a::TD) = isfinite(a.hi)
+isnan(a::DD)        = isnan(a.hi)
+isinf(a::DD)        = isinf(a.hi)
+isfinite(a::DD)     = isfinite(a.hi)
+issubnormal(a::DD)  = issubnormal(a.hi)
+
+iszero(a::DD)       = (a.hi == zero(typeof(a.hi)))
+ispos(a::DD)        = (a.hi >= zero(typeof(a.hi)))
+ispoz(a::DD)        = !signbit(a.hi)
+isneg(a::DD)        = signbit(a.hi)
+
+# comparatives
