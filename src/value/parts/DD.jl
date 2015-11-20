9ee54c25ea2e7f4b3ac13cdf2382abee26bac578
J-Sarnoff <jeffreysarnoff@gmail.com>
2015-11-20 16:57:24 -0500

    Update DD.jl

--- src/value/parts/DD.jl
+++ src/value/parts/DD.jl
@@ -1,12 +1,10 @@
-sign(a::DD)    = sign(a.hi)
-signbit(a::DD) = signbit(a.hi)
-
-function ldexp(a::DD,xp::Int)
-    DD(ldexp(a.hi,xp),ldexp(a.lo,xp))
-end
-
 function frexp(a::DD)
     frhi, xphi = frexp(a.hi)
     frlo, xplo = frexp(a.lo)
     DD(frhi, ldexp(frlo,xplo-xphi)), xphi
 end
+
+function ldexp(a::DD,xp::Int)
+    DD(ldexp(a.hi,xp),ldexp(a.lo,xp))
+end
+ldexp{I<:Integer}(fx::Tuple{DD,I}) = ldexp(fx...)
