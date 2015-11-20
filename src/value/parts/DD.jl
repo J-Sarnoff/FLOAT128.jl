2f135b0c00b3ce5dbd68236dbcaa8216677d6fdc
J-Sarnoff <jeffreysarnoff@gmail.com>
2015-11-20 02:06:13 -0500

    Create DD.jl

---
+++ src/value/parts/DD.jl
@@ -0,0 +1,9 @@
+function ldexp(a::DD,xp::Int)
+    DD(ldexp(a.hi,xp),ldexp(a.lo,xp))
+end
+
+function frexp(a::DD)
+    frhi, xphi = frexp(a.hi)
+    frlo, xplo = frexp(a.lo)
+    DD(frhi, ldexp(frlo,xplo-xphi)), xphi
+end
