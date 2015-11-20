453c49b75a9019d482458418813049ed6b51642e
J-Sarnoff <jeffreysarnoff@gmail.com>
2015-11-20 02:36:58 -0500

    Update TD.jl

--- src/value/parts/TD.jl
+++ src/value/parts/TD.jl
@@ -1,2 +1,13 @@
 sign(a::TD)    = sign(a.hi)
 signbit(a::TD) = signbit(a.hi)
+
+function ldexp(a::TD,xp::Int)
+    TD(ldexp(a.hi,xp),ldexp(a.md,xp),ldexp(a.lo,xp))
+end
+
+function frexp(a::DD)
+    frhi, xphi = frexp(a.hi)
+    frmd, xpmd = frexp(a.md)
+    frlo, xplo = frexp(a.lo)
+    TD(frhi, ldexp(frmd,xpmd-xphi), ldexp(frlo,xplo-xpmd-xphi)), (xphi, xpmd, xplo)
+end
