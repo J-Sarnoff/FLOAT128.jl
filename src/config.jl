88628fad234655288cce44d2d8c3ffe2ea3d84aa
J-Sarnoff <jeffreysarnoff@gmail.com>
2015-11-20 06:09:00 -0500

    Update config.jl

--- src/config.jl
+++ src/config.jl
@@ -1,6 +1,3 @@
 #=
-   The typealias *FloatingPoint* is used as a parameter for error free transformations.
-   If you want multiple Floating Point types available, use a Union here.
-   For most purposes, it should be either Float64 or, e.g. for CUDA Float32.
+   configurable.
 =#
-typealias FloatingPoint Float64
