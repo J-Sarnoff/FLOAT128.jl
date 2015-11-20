0e1b9d972cf7cd3a264a19ad0fd6af579ed366c9
J-Sarnoff <jeffreysarnoff@gmail.com>
2015-11-20 02:31:52 -0500

    Update DD.jl

--- src/value/cmp/DD.jl
+++ src/value/cmp/DD.jl
@@ -3,7 +3,7 @@
 isnan(a::DD)        = isnan(a.hi)
 isinf(a::DD)        = isinf(a.hi)
 isfinite(a::DD)     = isfinite(a.hi)
-issubnormal(a::DD)  = (issubnormal(a.hi) | issubnormal(a.lo))
+issubnormal(a::DD)  = issubnormal(a.hi)
 
 iszero(a::DD)       = (a.hi == zero(typeof(a.hi)))
 ispos(a::DD)        = (a.hi >= zero(typeof(a.hi)))
