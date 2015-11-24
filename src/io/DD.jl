3e2ec3229539a14be0b1c0132191242319a6c11d
J-Sarnoff <jeffreysarnoff@gmail.com>
2015-11-24 13:55:05 -0500

    Update DD.jl

--- src/io/DD.jl
+++ src/io/DD.jl
@@ -13,18 +13,19 @@ end
 
 function parse(::Type{DD}, str::AbstractString)
     if !(startswith(str,DD_typename))
-        throw(ErrorException("$(DD_typename) not found in $(str)"))
-    end
-    s = str[10:(end-1)]
-    hilo = split(s,',')
-    if length(hilo)==1
-        hilo = [hilo[1],"0.0"]
-    end
-    shi,slo = hilo[1],hilo[2]
-    hi = parse(Float64,shi)
-    lo = parse(Float64,slo)
-    hi,lo = eftSum2(hi,lo)
-    DD(hi,lo)
+        convert(DD, parse(BigFloat,str))
+    else
+        s = str[10:(end-1)]
+        hilo = split(s,',')
+        if length(hilo)==1
+            hilo = [hilo[1],"0.0"]
+        end
+        shi,slo = hilo[1],hilo[2]
+        hi = parse(Float64,shi)
+        lo = parse(Float64,slo)
+        hi,lo = eftSum2(hi,lo)
+        DD(hi,lo)
+    end    
 end
 
 function hex(x::DD)
